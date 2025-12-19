833d56e0ca517a25945464313bc36aadf5c5be2f
cg-Gantes <oigantes@gmail.com>
2025-12-19 17:47:50 +0800

    feat: -add api routes - add user seeder - created login endpoint

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $userAccount = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            if (!Auth::attempt($userAccount)) {
+                return response()->json(['message' => 'Invalid credentials'], 401);
+            }
+
+            $user = User::where('email', $userAccount['email'])->first();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json(['token' => $token], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Incorrect Email or Password',
+                'error' => $th->getMessage()
+            ], 401);
+        }
+    }
+}
